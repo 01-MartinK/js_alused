2c49ab0bf6d7fc126ea0481458fb3b2abfea1f4a
Martin K <thedeadgamer101@gmail.com>
2021-09-20 11:43:06 +0300

    Datatypes

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
- // muutujad
- // var, let, const
- // lubatud märgid nime loomisel - tähed, numbrid, _, $
- // numbriga muutuja nimi alustada ei tohi!
-let name = "mk";
-console.log(name);
+// Andmetüübid
 
-let int = 1;
-console.log(int);
+// Primitiivid
 
+// String - tekst
+const name = "MK";
+console.log(typeof name);
 
-function myFunction() {
-	int = int + 1;
-  document.getElementById("demo").innerHTML = int + " klicki tehtud";
-}
+// Number
+const age = 72;
+console.log(typeof age);
+
+// Float
+const percent = 32.3;
+console.log(typeof percent);
+
+// Boolean
+const has_kids = false;
+console.log(typeof has_kids);
+
+// Null
+const airplane = null;
+console.log(typeof airplane);
+
+// Undefined
+let test;
+console.log(typeof test);
+
+// Char
+const char = Symbol();
+console.log(typeof char);
+
+// Objects - referance types - tüübid, mille sisu saab viide kaudu
+// Array - massiiv
+const hobbies = ['movies', 'music'];
+console.log(typeof hobbies);
+
+// Object
+const person = {
+	name: "kadi",
+	age: 15
+};
+console.log(typeof person);
+
+// näiteks kuupäev objekt
+const today = new Date();
+console.log(typeof today);
+console.log(today.getHours());
