626aad9b4940e768b0c6e681972172c392e4a20e
Martin K <thedeadgamer101@gmail.com>
2021-09-20 11:29:28 +0300

    Simple javascript variables and a function

---
+++ app.js
@@ -0,0 +1,15 @@
+ // muutujad
+ // var, let, const
+ // lubatud märgid nime loomisel - tähed, numbrid, _, $
+ // numbriga muutuja nimi alustada ei tohi!
+let name = "mk";
+console.log(name);
+
+let int = 1;
+console.log(int);
+
+
+function myFunction() {
+	int = int + 1;
+  document.getElementById("demo").innerHTML = int + " klicki tehtud";
+}
