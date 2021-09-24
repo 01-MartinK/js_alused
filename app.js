495928d4f1a8d1652bc9feeefbd2d5b76462cc0c
Martin K <thedeadgamer101@gmail.com>
2021-09-24 09:40:38 +0300

    Arrays

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-const name = "mk";
-const age = 25;
-const job = "student";
-const city = "tartu";
-const hobbies = ["programming","boxing","nature"];
+const numbers = [43, 62, 213, 43, 54, 56, 1, 23];
+const numbers2 = new Array(22,765,43,21,12);
+const fruits = ['apple','pair','banana','dragonfruit']
+const mixed = [22, 'tere', true, undefined, null, {a:1,v:2}, new Date()];
 
-let html = '<ul>' + 
-				'<li>' + name + '</li>' + 
-				'<li>' + age + '</li>' + 
-				'<li>' + job + '</li>' + 
-				'<li>' + city + '</li>' + 
-				'<li>' + hobbies + '</li>' + 
-			'</ul>';
+let val;
 
-console.log(html);
+val = numbers.indexOf(1); // leidmine indexi vaartuse elemendi kaudu
+numbers2[4] = 62; // elemendi muutmine
 
-document.body.innerHTML += html;
+numbers2.push(72);
+numbers2.unshift(54);
+numbers2.pop();
+numbers2.shift();
+console.log(numbers2);
 
-// es6 - template string
+//numbers2.splice(1,3); loikamine
 
-html = `
-	<ul>
-		<li>Name: ${name}</li>
-		<li>Age: ${age}</li>
-		<li>Job: ${job}</li>
-		<li>City: ${city}</li>
-		<li>Hobbies: ${hobbies}</li>
-	</ul>
-	`;
+//numbers2.reverse(); umberpooramine
 
-console.log(html);
+let kokku = numbers.concat(numbers2); // massiivide liitmine
 
-document.body.innerHTML += html;
+console.log(kokku);
+console.log(Array.isArray(numbers2)); // massiivi kontrollimine
+console.log(numbers);
+
+// massiivi sorteerimine
+numbers2.sort(function(x, y){ 
+	return y - x;
+});
+
+// String massiivi sorteerimine
+console.log(fruits.sort());
+
+console.log(numbers2);
+console.log(val);
