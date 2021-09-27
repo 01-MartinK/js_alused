147d79a356863b568593c94c349eef203c3dba07
Martin K <thedeadgamer101@gmail.com>
2021-09-27 11:26:31 +0300

    if and else statements and stuff

--- app.js
+++ app.js
@@ -1,19 +1,67 @@
-let val;
+const id = 101;
 
-const today = new Date();
+// == - vordub
+// != - ei vordu
+// <= - suurem
+// >= - vaiksem
+// === - vordub ja tuupikontroll
+// !== - ei vordu ja tuubi kontroll
 
-val = today.getMonth(); // starts at january 0
-val = today.getDate();
-val = today.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
-val = today.getTime();
+/*
+let testID;
 
-let birthday = new Date('9-27-2001 11:00:00');
+if (id > 100){
+	console.log(`id on ${id} - suurem kui 100`);
+}else if (id < 100) {
+	console.log(`id on ${id} - vaiksem kui 100`);
+}
+*/
 
-console.log(birthday);
-console.log(today);
-console.log(val);
+// if - else if - else
+
+const color = "gold";
+
+if (color === 'gold'){
+	console.log("ready");
+} else if (color === 'red'){
+	console.log("stop");
+} else if (color === 'green'){
+	console.log("go");
+} else{
+	console.log("color not accepted");
+}
+
+for(let i = 0; i<320;i++){
+	console.log("hello");
+}
+
+// and - &&
+const name = "Kadi";
+const age = 32;
+
+if (age > 0 && age <= 12){
+	console.log(`${name} on laps`);
+} else if (age > 12 && age <= 18){
+	console.log(`${name} on nooruk`);
+} else {
+	console.log(`${name} on taiskasvanu`);
+}
+
+// or - ||
+
+if (age < 18 || age > 65){
+	console.log(`${name} ei saa registeerida`)
+} else {
+	console.log(`${name} saab registeerida`)
+}
+
+// ? - ternary operaator
+
+console.log(id === 100 ? 'oige' : 'vale')
+
+// ilma {}
+
+if (id === 50)
+	console.log('On 50');
+else
+	console.log('Ei ole 50');
