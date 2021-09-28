1f8cdabb7afd33cd62d7c10c26fb5b648449ca00
Martin K <thedeadgamer101@gmail.com>
2021-09-28 11:44:03 +0300

    Elemendi haaramine

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 let val;
 
-val = document.head;
-val = document.body;
+val = document.getElementById("titel");
+val = document.getElementById("titel").id;
+val = document.getElementById("titel").className;
 
-console.log(val);
+const taskTitle = document.getElementById("titel");
+
+taskTitle.style.background = '#333';
+taskTitle.style.color = '#fff';
+taskTitle.style.padding = '5px';
+
+taskTitle.textContent = 'task list';
+
+val = document.querySelector('#titel');
+val = document.querySelector('.contentbox2');
+val = document.querySelector('h2');
+
+document.querySelector('p').style.background = 'red';
+
+document.querySelector('h2:last-child').style.background = 'green';
+
+console.log(val)
