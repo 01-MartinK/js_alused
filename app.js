f446eaaddae40c5dedd9e45a431c3996286ec87c
Martin K <thedeadgamer101@gmail.com>
2021-09-29 15:14:41 +0300

    Dom traversing

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 let val;
 
-val = document.querySelectorAll('p');
+const list = document.querySelector('div');
+const listItem = document.querySelector('p:first-child')
 
-const oddP = document.querySelectorAll('p:nth-child(odd)');
+val = list;
+val = listItem;
 
-const evenP = document.querySelectorAll('p:nth-child(even)');
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[0].nodeType;
 
-oddP.forEach(function(li){
-	li.style.background = "#999999";
-});
+val = list.children;
+val = list.children[1];
+//list.children[1].textContent = 'study xml';
+
+val = list.children[1].children[0].children;
+list.children[1].children[0].id = "test-id";
+
+val = list.firstChild;
+val = list.firstElementChild;
+
+val = list.LastChild;
+val = list.lastElementChild;
+
+val = list.childElementCount;
+
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
+
+val = listItem.nextSibling;
+val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
 
 console.log(val);
