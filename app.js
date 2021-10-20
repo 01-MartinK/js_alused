ebbe8eafb444f16969132787a1d9431c32adaa14
Martin K <thedeadgamer101@gmail.com>
2021-10-20 10:53:58 +0300

    async and await torture

--- app.js
+++ app.js
@@ -1,51 +1,33 @@
-let books = [];
+// thenable
+class Thenable {
+    constructor(num) {
+      this.num = num;
+    }
+    then(resolve, reject) {
+      alert(resolve);
+      // resolve with this.num*2 after 1000ms
+      setTimeout(() => resolve(this.num * 2), 1000); // (*)
+    }
+  }
+  
+  async function f() {
+    // waits for 1 second, then result becomes 2
+    let result = await new Thenable(1);
+    alert(result);
+  }
+  
+  f();
 
-const nameValueHolder = document.querySelector("#nameInput");
-const autorValueHolder = document.querySelector("#autorInput");
-const codeHolder = document.querySelector("#codeInput");
-const submitBtn = document.querySelector("#btn")
+  // await
+  async function c() {
 
-const saveManager = new storageManager;
-const UI = new UIManager;
-
-startLoad()
-
-function arrayRemove(arr, value) { 
-
-    return arr.filter(function(ele){ 
-        return ele.name != value; 
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve("done!"), 1000)
     });
-}
-
-function submitBook(){
-    console.log("submitted");
-    UI.alertMessage("Submitted Book");
-    UI.addBook(nameValueHolder.value,autorValueHolder.value,codeHolder.value);
-    saveBooks();
-} 
-
-// saving the books to local storage
-function saveBooks(){
-    saveManager.saveAttributes(books);
-}  
-
-// deleting a book
-function deleteBook(deletable){
-    const table = document.querySelector("#table");
-
-    table.removeChild(deletable.parentElement.parentElement.parentElement);
-    books = arrayRemove(books, deletable.id);
-    saveBooks();
-    UI.alertMessage("Deleted a Book");
-}
-
-function startLoad(){
-    let list = saveManager.getAttributes("books");
-    for(let i = 0;i<list.length;i++)
-    {
-        UI.addBook(list[i].name, list[i].author, list[i].code);
-    } 
-    books = list;
-    console.log(books);
-    UI.alertMessage("Loaded Library");
-}
+  
+    let result = await promise; // wait until the promise resolves (*)
+  
+    alert(result); // "done!"
+  }
+  
+  c();
